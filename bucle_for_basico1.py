43f6adbedc1a4ad2d95f30d0e231af6906dcb3b8
MATIAS ALEJANDRO GOMEZ <38659006+matigomez32@users.noreply.github.com>
2026-04-07 19:49:53 -0300

    Add files via upload

---
+++ bucle_for_basico1.py
@@ -0,0 +1,48 @@
+# 1)imprime todos los números enteros del 0 al 100.
+for num in range(0,101):
+    print(num)
+
+#2) imprime todos los números múltiplos de 2 entre 2 y 500
+
+for i in range(2,501,2):
+    print(i)
+
+#  3)imprime los números enteros del 1 al 100. Si es divisible por 5 
+    # imprime “ice ice” en vez del número.
+   # Si es divisible por 10, imprime “baby”
+
+for numero in range(1,101):
+    if numero%10==0:
+        print("baby")
+    elif numero%5==0:
+        print("ice ice")
+    else:
+        print(numero)
+
+#4) suma los números pares del 0 al 500,000 e imprime la suma total. 
+
+sumTotal=0
+for numerosPares in range(0,500001,2):
+    sumTotal+=numerosPares
+
+print("La suma total de los numeros pares es:",(sumTotal))
+
+
+#5)Regrésame al 3: imprime los números positivos comenzando desde 2024, en cuenta regresiva de 3 en 3.
+
+for numerosPositivos in range(2024,0,-3):
+    print(numerosPositivos)
+
+#6)Contador dinámico: establece tres variables: numInicial, numFinal y multiplo. 
+#Comenzando en numInicial y pasando por numFinal, imprime los números enteros que sean múltiplos de 
+#multiplo. Por ejemplo: si numInicial = 3, numFinal = 10, y multiplo = 2,
+#el bucle debería de imprimir 4, 6, 8, 10 (en líneas sucesivas).
+
+numInicial = 3
+numFinal = 10
+multiplo = 2
+
+for n in range(numInicial, numFinal + 1):
+    if n % multiplo == 0:
+        print(n)
+
